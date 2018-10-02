0dbe693310ce57ba284ca3d077fd29a0420454c6
Floyd Kots <floydkots@gmail.com>
2018-10-02 09:52:32 +0300

    Add the prototype pattern

---
+++ src/main/java/prototype_pattern/Rectangle.java
@@ -0,0 +1,15 @@
+package prototype_pattern;
+
+public class Rectangle extends Shape {
+
+    public Rectangle() {
+        type = "Rectangle";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+}
+
+
